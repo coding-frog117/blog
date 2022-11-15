c03e696767c438a818f66a307f3bb73dd99b2fc2
송예은 <110962765+coding-frog117@users.noreply.github.com>
2022-11-15 23:41:41 +0900

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import { useState } from 'react';
 function App() { 
   let [블로그제목,b]=useState('개굴공주의 블로그');
   let [title,setTitle]=useState(['겨울 아우터 추천','강남 맛집 추천','크리스마스 축제']);
-  let [like, likeChange]=useState(0);
+  let [like, likeChange]=useState([0,0,0]);
+  let [modal,setModal]=useState(false);
+  let [modalTitle,setModalTitle]=useState(0);
 
   return ( 
     <div className="App">
@@ -20,7 +22,7 @@ function App() {
         
       </div>
 
-      <div className="list">
+      {/* <div className="list">
         <h4>{title[0]} 
         <span onClick={()=>{
           likeChange(like+1)}}>
@@ -33,20 +35,52 @@ function App() {
           setTitle(copy);
           }}>
             여성</button>
-      </div>
+      </div> */}
 
+      {
+        title.map((a,i)=>{ 
+          return (
+          <div className="list">
+            <h4 onClick={()=>{
+              setModal(!modal);
+            }}> {title[i]} <span onClick= {()=>{let copy=[...like];
+            copy[i]=copy[i]+1;
+            likeChange(copy[i])
+            }}> 좋아요 </span>{like[i]}
+            </h4>
+            <p>11월 11일 발행 빼빼로데이!</p>
+          </div>
+        )
+        })
+      }             
 
-      <div className="list">
-        <h4>{title[1]}</h4>
-        <p>11월 11일 발행 빼빼로데이!</p>
-      </div>
+      {
+        modal == true ? <Modal title={title} setTitle={setTitle} color={'skyblue'} modalTitle={modalTitle}></Modal> :null
+      }
 
-      <div className="list">
-        <h4>{title[2]}</h4>
-        <p>11월 11일 발행 빼빼로데이!</p>
-      </div>
     </div>
   );
+}
+
+function Modal(props){
+  return (
+    <>
+      <div className="modal" style={{background:props.color}}>
+        <h4>{props.title[props.modalTitle]}
+        </h4>
+        <h4>날짜</h4>
+        <p>상세내용</p>
+      </div>
+
+      <Footer></Footer>
+    </>
+  )}
+
+
+function Footer(){
+  return(    
+     <small>이 글의 저작권은 개굴공주에게 있습니다</small> 
+  )
 }
 
 export default App;
